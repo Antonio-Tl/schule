7fbde4670d97030356e02b81ce90651dcbce77ea
Antonio <j.antonio.tlusteck@gmx.de>
2022-01-28 11:51:49 +0100

    fixed Address (listA, formularA, formularAAction, ActionDeleteA)

--- listA.php
+++ listA.php
@@ -21,28 +21,41 @@ require_once 'classes/Subject.php';
     <tr>
         <th>Löschen</th>
         <th>ID</th>
+        <th>Vorname</th>
+        <th>Nachname</th>
         <th>Postleizahl</th>
         <th>Stadt</th>
         <th>Straße</th>
         <th>Hausnummer</th>
     </tr>
     <?php
-    $sql = "SELECT * from schule.adresse WHERE deleted_at is NULL";
+    query('use schule');
+    $sql = "SELECT CONCAT(AP.a_id,'-',AP.s_id) as id, S.fname, S.lname, A.plz, A.stadt, A.strasse, A.nr 
+            from schule.adresse A
+            JOIN adresse_schueler AP ON A.id = AP.a_id
+            JOIN schule.schueler S ON S.id = AP.s_id
+            WHERE S.deleted_at is NULL and A.deleted_at is null and AP.deleted_at is null";
+    echo $sql;
     $result = query($sql);
-    $row = '<tr> <td>###DELETEACTION###</td><td>###ID###</td><td>###POSTLEIZAHL###</td><td>###STADT###</td>
+    print_r($result);
+    $row = '<tr> <td>###DELETEACTION###</td><td>###ID###</td><td>###FNAME###</td><td>###LNAME###</td><td>###POSTLEIZAHL###</td><td>###STADT###</td>
                  <td>###STRASSE###</td><td>###HAUSNUMMER###</td></tr>';
     foreach ($result as $dataRow) {
         echo str_replace(
             array(
                 '###DELETEACTION###',
                 '###ID###',
+                '###FNAME###',
+                '###LNAME###',
                 '###POSTLEIZAHL###',
                 '###STADT###',
                 '###STRASSE###',
-                '###HAUSNUMMER###'),
+                '###HAUSNUMMER###',),
             array(
                 '<A href="ActionDeleteS.php?id='.$dataRow['id'].'">[X]</A>',
                 $dataRow['id'],
+                $dataRow['fname'],
+                $dataRow['lname'],
                 $dataRow['plz'],
                 $dataRow['stadt'],
                 $dataRow['strasse'],
--- listC.php
+++ listC.php
@@ -81,7 +81,7 @@ function getCourseTable($courseId)
 
     query('use schule');
     $sql = "SELECT CONCAT(KS.k_id,'-',KS.s_id) as id, S.fname, S.lname, KS.note from schule.kurse K  JOIN kurse_schueler KS ON K.id=KS.k_id ";
-    $sql .="JOIN schueler s on KS.s_id = s.id WHERE S.deleted_at is NULL and K.deleted_at is null and KS.deleted_at is null";
+    $sql .="JOIN schueler S on KS.s_id = s.id WHERE S.deleted_at is NULL and K.deleted_at is null and KS.deleted_at is null";
 
     $result = query($sql);
 
